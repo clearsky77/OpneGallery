dbfdce7c1e9a38db0a691028de9bbebf78bb5e1f
clearsky77 <clearsky3264@gmail.com>
2021-10-09 23:12:31 +0900

    권한 확인 창 띄우기. TedPermission(라이브러리) 활용.

--- app/src/main/java/com/clearsky77/opnegallery/MainActivity.kt
+++ app/src/main/java/com/clearsky77/opnegallery/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.clearsky77.opnegallery
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        openGalleyBtn.setOnClickListener {
+//          권한 승인 여부에 따른 규칙 정리.
+            val pl = object : PermissionListener {
+                // 권한이 허용 되었을 때. 실행한다.
+                override fun onPermissionGranted() {
+                    Toast.makeText(this@MainActivity, "권한 승인됨.", Toast.LENGTH_SHORT).show()
+                }
+                // 권한 거절 되었을 때.
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(this@MainActivity, "권한 거절됨.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE) // 저장소 쓰기 권한
+//                .setPermissions(Manifest.permission.READ_EXTERNAL_STORAGE) // 저장소 읽기 권한 <- 하나만 있어도 된다고 함
+                .check()
+        }
     }
 }
